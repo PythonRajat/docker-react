6adb6b50b111da2cd5b04d9fe36dfabbf249c8af
Ubuntu <ubuntu@ip-172-31-5-206.ap-south-1.compute.internal>
2024-05-18 12:02:08 +0000

    intial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome Rajat Chaudhari to nodejs with React App on Docker volume
         </p>
         <a
           className="App-link"
